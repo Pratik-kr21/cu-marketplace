6b0b1c2dfc44e0f47fc8b76cb0c9fe5e80ae8757
Pratik Kumar <pratik792584@gmail.com>
2026-03-03 22:41:14 +0530

    fix: email verification token issue

--- server/models/User.js
+++ server/models/User.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
     hostel: { type: String, default: '' },
     avatar_url: { type: String, default: null },
     isVerified: { type: Boolean, default: false },
+    emailVerificationToken: { type: String, default: null },
     emailVerificationExpires: { type: Date, default: null },
     resetPasswordToken: { type: String, default: null },
     resetPasswordExpires: { type: Date, default: null },
