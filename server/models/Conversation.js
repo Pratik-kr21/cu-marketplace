5c1d693ffccafeeaf632606eebb20df319879153
Pratik Kumar <pratik792584@gmail.com>
2026-05-10 03:57:23 +0530

    Configure 24h message TTL and 1-week conversation TTL

--- server/models/Conversation.js
+++ server/models/Conversation.js
@@ -9,4 +9,8 @@ const conversationSchema = new mongoose.Schema({
 // Unique compound index: one conversation per buyer+item
 conversationSchema.index({ item_id: 1, buyer_id: 1 }, { unique: true })
 
+// Automatically delete conversation after 1 week of inactivity
+// 604800 seconds = 1 week
+conversationSchema.index({ updatedAt: 1 }, { expireAfterSeconds: 604800 })
+
 export default mongoose.model('Conversation', conversationSchema)
--- server/models/Message.js
+++ server/models/Message.js
@@ -8,8 +8,8 @@ const messageSchema = new mongoose.Schema({
     read: { type: Boolean, default: false },
 }, { timestamps: true })
 
-// Create a TTL index to automatically delete messages 1 week after they are created
-// 604800 seconds = 1 week
-messageSchema.index({ createdAt: 1 }, { expireAfterSeconds: 604800 })
+// Create a TTL index to automatically delete messages 24 hours after they are created
+// 86400 seconds = 24 hours
+messageSchema.index({ createdAt: 1 }, { expireAfterSeconds: 86400 })
 
 export default mongoose.model('Message', messageSchema)
